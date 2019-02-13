fc9fb199cdf28ee1b8517936db132edef301782e
Jorge Chantres <jorge.chantres@hotmail.com>
2019-02-12 19:59:22 -0600

    Chamba bien hecho, Drag and Drop desde slide, Show up de infromacion a comparar

---
+++ js/DragandDrop.js
@@ -0,0 +1,34 @@
+
+
+function allowDrop(ev)  {
+    ev.preventDefault();
+
+  }
+  
+  function drag(ev)  {
+     ev.dataTransfer.setData("text", ev.target.id);
+     var dato = ev.dataTransfer.getData("text");
+     
+  }
+  
+  function drop(ev)  {
+    var dato = ev.dataTransfer.getData("text");
+    var dato1 = ev.target.id;
+     ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    
+
+    if(dato == 'drag1'){
+    document.getElementById('scrolling').style.display = "contents";
+    document.getElementById('div1').style.backgroundImage = "url('Imagenes/iphonecarac.jpg')";
+    document.getElementById('scrolling').style.backgroundImage = "url('Imagenes/volver.jpg')";
+    
+
+
+    
+  }
+    
+    if(dato == 'drag2'){
+      document.getElementById('div1').style.background='black';}
+  }
+     
