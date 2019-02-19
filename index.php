d7171315142d4860dddad47f5e56e0037d3bcf1f
albertrs <sheik.albert@gmail.com>
2019-02-19 17:16:27 -0600

    Se modificaron los estilos

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title></title>
+	<script src="js/jquery-3.3.1.slim.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
